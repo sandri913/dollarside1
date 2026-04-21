e65260f31e7838d33294f9b513dc90f9ab8b22bb
dovodi39-stlkr <davideschiavo2317@gmail.com>
2026-04-21 11:27:30 +0200

    Add function declarations in DollarProject.cpp
    Added function declarations for various operations.

--- DollarProject.cpp
+++ DollarProject.cpp
@@ -2,6 +2,14 @@
 #include<cstdlib>
 #include<cstream>
 using namespace std;
+int saldo();
+int mercato();
+int traders();
+int prelievo();
+int lavori();
+int Shop();
+int deposito();
+
 int main()
 {
   return 0;
