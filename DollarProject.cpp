ba3b2b21e92b4681b362089aebea0297caa0e5c3
sandri913 <eliabmx913@gmail.com>
2026-05-05 10:57:23 +0200

    Update DollarProject.cpp

--- DollarProject.cpp
+++ DollarProject.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<cstdlib>
 #include<sstream>
+#include<ctime>
+
 using namespace std;
 
+int s;
 int saldo();
 int mercato();
 int traders();
@@ -16,71 +19,56 @@ int risposta1;
 
 int main()
 {
-cout<<"\n _____     ______     __         __         ______     ______     ______     __     _____     ______";    
-cout<<"\n/\  __-.  /\  __ \   /\ \       /\ \       /\  __ \   /\  == \   /\  ___\   /\ \   /\  __-.  /\  ___\";   
-cout<<"\n\ \ \/\ \ \ \ \/\ \  \ \ \____  \ \ \____  \ \  __ \  \ \  __<   \ \___  \  \ \ \  \ \ \/\ \ \ \  __\";   
-cout<<"\n \ \____-  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\  \ \_\ \_\  \/\_____\  \ \_\  \ \____-  \ \_____\"; 
-cout<<"\n  \/____/   \/_____/   \/_____/   \/_____/   \/_/\/_/   \/_/ /_/   \/_____/   \/_/   \/____/   \/_____/"; 
-                                                                                                     
+
  cout<<"\nScrivi la Password (solo con lettere)\n";
  cin>>password;
  cout<<"\nScrivi nome utente (senza spazi)\n";
  cin>>nu;
  system("cls");
  cout<<"\nBenvenuti al menu,grazie di averci supportato...\n";
- cout<<"1) Saldo       ";
- cout<<"2) Mercato     ";
- cout<<"3) traders     ";
- cout<<"4) Prelievo    ";
- cout<<"5) Lavori      ";
- cout<<"6) Shop        ";
- cout<<"7) Deposito    ";
-switch(risposta1)
+ cout<<"\n1) Saldo       ";
+ cout<<"\n2) Mercato     ";
+ cout<<"\n3) traders     ";
+ cout<<"\n4) Prelievo    ";
+ cout<<"\n5) Lavori      ";
+ cout<<"\n6) Shop        ";
+ cout<<"\n7) Deposito    ";
+do
 {
-  case 1:
-  saldo();
-  break;
-  case 2:
-  mercato();
-  break;
-  case 3:
-  traders();
-  break;
-  case 4:
-  prelievo();
-  break;
-  case 5:
-  lavori();
-  break;
-  case 6:
-  Shop();
-  break;
-  case 7:
-  deposito();
-  break;
-  default:
-  risposta1<1||risposta>7
-  break;
- return 0;
+	cout<<"\n inserisci quello che vuoi eseguire!(numero)";
+	cin>>s;
+	
 }
-int saldo()
+while( s<1 || s>7);
+
+
+
+switch(s)
 {
-cout<<"\n     __     ____             _    __   ";
-cout<<"\n ___/ /__  / / /__ ________ (_)__/ /__ ";
-cout<<"\n/ _  / _ \/ / / _ `/ __(_-</ / _  / -_)";
-cout<<"\n\_,_/\___/_/_/\_,_/_/ /___/_/\_,_/\__/ ";                                       
-  cout<<"\nBuongiorno!";
-  cout<<"\nil tuo saldo attuale e' di"
-  //visualizzare il saldo,inserire un pop up che ti porta al programma mercato per aumentare oppure negozio(home)
-  return 0;
-  
+	case 1:
+		cout<<"hai scelto il saldo!";
+	break;
+	case 2:
+		cout<<"hai scelto il mercato!";
+	break;
+	case 3:
+		cout<<"hai scelto i traders!";
+	break;
+	case 4:
+		cout<<"hai scelto il prelievo!";
+	break;
+	case 5:
+		cout<<"hai scelto i lavori!";
+	break;
+	case 6:
+		cout<<"hai scelto lo shop!";
+	break;
+	case 7:
+		cout<<"hai scelto il deposito!";
+	break;
+	default:
+	break;	
 }
-cout<<"\n     __     ____             _    __   ";
-cout<<"\n ___/ /____/ / /__ ________ (_)__/ /__ ";
-cout<<"\n/ _  / _  / / / _ `/ __(_-</ / _  / -_)";
-cout<<"\n\_,_/\___/_/_/\_,_/_/ /___/_/\_,_/\__/
-  cout<<"\n     __     ____             _    __   ";
-cout<<"\n ___/ /____/ / /__ ________ (_)__/ /__ ";
-cout<<"\n/ _  / _  / / / _ `/ __(_-</ / _  / -_)";
-cout<<"\n|_,_/____/_/_/__,_/_/ /___/_/__,_/___/ ";                                       
-  cout<<"\nBuongiorno!";
+
+return 0;
+}
